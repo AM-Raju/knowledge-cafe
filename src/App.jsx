930a199f2c772398cab2324364554777afcc7712
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-03-31 22:20:41 +0600

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Blog from "./components/Blog/Blog";
 import Header from "./components/Header/Header";
+import QAsection from "./components/QAsection/QAsection";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
-import QAsection from "./components/QAsection/QAsection";
 
 const iconList = Object.keys(Icons)
   .filter((key) => key !== "fas" && key !== "prefix")
